399bb4cedc1ff1a4a7c901f4627b86417248c77c
Mohamed Gamal <eng.mg2011@gmail.com>
2022-12-01 18:09:46 +0300

    feat: read file using native js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import FileUpload from "./components/FileUpload";
+import {useRef} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let fileInfo = useRef(null);
+    const processFile = (file) => {
+        // Checking modern browsers
+        if ( !(window.File && window.FileReader && window.FileList && window.Blob) )
+            alert("Please use browser supports HTML5 File API");
+
+        const reader = new FileReader();
+        if (!file.type.match(/text.csv/))
+            fileInfo.current.innerHTML = "<span class='error'>It doesn't seem to be a text file!</span>";
+
+        reader.onload = (event) => {
+            fileInfo.current.innerHTML = event.target.result;
+        }
+        reader.readAsText(file);
+
+    }
+
+    return (
+        <div className="App">
+            <FileUpload processFile={processFile}/>
+
+            <div ref={fileInfo}>
+
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
